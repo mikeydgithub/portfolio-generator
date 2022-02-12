f887a177b9a416d53a8986d6c11a46c7051cfffd
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-02-12 18:52:18 -0500

    added the questions with answers. saved ansewers to an array. and prompted to add additional projects

--- app.js
+++ app.js
@@ -1,14 +1,89 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
         type: 'input',
         name: 'name',
         message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
     }
-    ])
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+==================
+Add a New Project 
+==================   
+`);
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'about',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with?(Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            defualt: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmedAddProject',
+            message: 'Would you like to enter another project?',
+            defualt: false
+        }
+        ])
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+            if (projectData.confirmedAddProject){
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        });
+
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+
 
 // The *require* statment is a built-in function thats globally available in node.js. 
 // It allows the app.js file to access the fs module's functions through the 'fs' assignment.
