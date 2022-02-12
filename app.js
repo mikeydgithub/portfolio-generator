3028f950a72e6de1ccd2f19e02aa4df5fab12e35
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-02-12 17:39:43 -0500

    installed inquirer npm. put it into the app.js as a required. setup a prompt from the inquirer npm

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+    ])
+    .then(answers => console.log(answers));
+
 // The *require* statment is a built-in function thats globally available in node.js. 
 // It allows the app.js file to access the fs module's functions through the 'fs' assignment.
-const fs = require('fs'); 
+//const fs = require('fs'); 
 // code modules
 // file systems modules - allows, create, and delete files on the server
 // local module - which are the module you wright
 // third party modules - modules you get from the web
-const generatePage = require('./src/page-template.js')
-console.log(generatePage);
+//const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github)
 
-const [name, github] = profileDataArgs;
-
-// (1) fs or filesyetem is creating a (2) writefile (3) creating the name of the file 'index.html' (4) data is set to generatePage function 
+// (1) fs or filesyetem is creating a (2) writefile (3) creating the name of the file 'index.html' (4) data is set to pageHTML function 
 // (5) passing in name and github (6) the third arguement will handle any errors as well as sucess message.
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw (err);
     
-    console.log('Portfolio complete! Check out index.html see the output!')
-});
+//     console.log('Portfolio complete! Check out index.html see the output!')
+// });
